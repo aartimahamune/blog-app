d09470e20d25adba41954a5b018be0d8a7c1ea0a
aartimahamune <aartimahamune5@gmail.com>
2024-06-22 20:08:24 +0530

    install reactJS and created the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="app-text">Hello, App!</h1>
+    </div>
+  );
+}
